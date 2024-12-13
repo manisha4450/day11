b41cec340df58ee38158617a54f893ef97614543
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 13:59:32 +0530

    Add files via upload

---
+++ hm1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class hm1{
+    public static void main(String[] args){
+        HashMap<String,String>a=new HashMap<>();
+        a.put("SNO","01");
+        a.put("SNAME","Muraja");
+        System.out.println(a);
+    }
+}
